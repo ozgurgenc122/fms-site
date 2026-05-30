2b8e6c04cbab3b59b5f06d3c08958bb1fe7380f6
Özgür <ozgurrrgenc123@gmail.com>
2026-05-30 21:18:16 +0300

    Evrak erisimi login korumali yapildi

--- istakip/urls.py
+++ istakip/urls.py
@@ -1,17 +1,17 @@
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.contrib.auth.decorators import login_required
 from django.urls import include, path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("login/", auth_views.LoginView.as_view(template_name="takip/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(next_page="login"), name="logout"),
+
+    # Evraklar sadece giriş yapan kullanıcıya açılsın
+    path("media/<path:path>", login_required(serve), {"document_root": settings.MEDIA_ROOT}),
+
     path("", include("takip.urls")),
 ]
-
-# Media dosyalarını sadece yerel geliştirmede Django servis etsin.
-# Canlıda media için Cloudinary / web server kullanılmalı.
-
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
